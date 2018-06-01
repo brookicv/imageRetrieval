e43a328c8387e5c1035a7bc4589eff401dda2fd5
Brook <brookicv@gmail.com>
2018-06-01 11:07:58 -0700

    Implementing sift and root-sift extractor using vlfeat

--- imageRetrieval.cpp
+++ imageRetrieval.cpp
@@ -3,8 +3,9 @@
 #include <opencv2/xfeatures2d/nonfree.hpp>
 
 #include <string>
-
-#include <vl/sift.h>
+extern "C" {
+    #include <vl/sift.h>
+}
 
 using namespace std;
 using namespace cv;
@@ -36,9 +37,74 @@ void bow()
     }
 }
 
+/*
+    Extract sift using vlfeat
+    parameters:
+        vl_sfit, VlSiftFilt* 
+        data , image pixel data ,to be convert to float
+        kpts, keypoint list
+        descriptors, descriptor. Need to free the memory after using.
+*/
+void vl_sift_extract(VlSiftFilt *vl_sift, vl_sift_pix* data,
+                    vector<VlSiftKeypoint> &kpts,vector<float*> &descriptors) {
+    
+    // Detect keypoint and compute descriptor in each octave
+    if(vl_sift_process_first_octave(vl_sift,data) != VL_ERR_EOF){
+        while(true){
+            vl_sift_detect(vl_sift);
+
+            VlSiftKeypoint* pKpts = vl_sift->keys;
+            for(int i = 0; i < vl_sift->nkeys; i ++) {
+                
+                kpts.push_back(*pKpts);
+
+                double angles[4];
+                // 计算特征点的方向，包括主方向和辅方向，最多4个
+                int angleCount = vl_sift_calc_keypoint_orientations(vl_sift,angles,pKpts);
+
+                // 只计算主方向的描述子
+                float *des = new float[128];
+                vl_sift_calc_keypoint_descriptor(vl_sift,des,pKpts,angles[0]);
+                descriptors.push_back(des);
+                pKpts ++;
+            }    
+            // Process next octave
+            if(vl_sift_process_next_octave(vl_sift) == VL_ERR_EOF) {
+                break ;
+            }
+        }
+    }
+}
+
+void convert_sift_to_root_sift(vector<float*> &descriptors){
+        /*
+        Trans sift to rootSift
+        1. L1 normalize each descriptor(128-dims vector)
+        2. sqrt-root each element
+        3. [option] L2-normalize descriptor vector
+     */
+    for(float* ptr : descriptors) {
+        float sum = 0;
+        for(int i = 0; i < 128; i ++)
+            sum += *(ptr + i);
+        for(int i = 0; i < 128; i ++){
+            *(ptr + i) /= sum;
+            *(ptr + i) = sqrt(*(ptr + i));
+        }
+    }
+}
+
+void vl_root_sift_extract(VlSiftFilt *vl_sift, vl_sift_pix* data,vector<VlSiftKeypoint> &kpts,vector<float*> &descriptors) {
+
+    vl_sift_extract(vl_sift,data,kpts,descriptors);
+    convert_sift_to_root_sift(descriptors);
+}
+
+
+
 int main() 
 {
-    const string file = "/home/brook/projects/ImageRetrievalService/imageRetrieval/images/000014.jpg";
+    const string file = "/home/book/Pictures/timg.jpeg";
     Mat img = imread(file,IMREAD_GRAYSCALE);
     Mat color_img = imread(file);
     Mat float_img;
@@ -54,35 +120,10 @@ int main()
 
 
     vector<VlSiftKeypoint> kpts;
-    vector<KeyPoint> oc_kpts;
     vector<float*> descriptors;
-    int octave_idx = 0;
-    if(vl_sift_process_first_octave(vl_sift,data) != VL_ERR_EOF) {
-        while(true) {
-            vl_sift_detect(vl_sift);
 
-            KeyPoint oc_kpt;
-            oc_kpt.octave = octave_idx;
-            VlSiftKeypoint *pKpts = vl_sift->keys;
-            for(int i = 0; i < vl_sift->nkeys; i ++) {
-                kpts.push_back(*pKpts);
-                oc_kpt.pt = Point(pKpts->x,pKpts->y);
-                double angles[4];
-                // 计算特征点的方向，包括主方向和辅方向，最多4个
-                int angleCount = vl_sift_calc_keypoint_orientations(vl_sift,angles,pKpts);
+    vl_sift_extract(vl_sift,data,kpts,descriptors);
 
-                // 只计算主方向的描述子
-                float *des = new float[128];
-                vl_sift_calc_keypoint_descriptor(vl_sift,des,pKpts,angles[0]);
-                descriptors.push_back(des);
-                pKpts ++;
-            }
-
-            if(vl_sift_process_next_octave(vl_sift) == VL_ERR_EOF) {
-                break ;
-            }
-        }
-    }
     vl_sift_delete(vl_sift);
 
     for(int i = 0; i < kpts.size(); i ++) {
