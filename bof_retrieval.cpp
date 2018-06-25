b7a7193d86bab95e2a83bfd082ead80f4e240411
Brook <brookicv@gmail.com>
2018-06-25 09:55:14 -0700

    flann search demo

--- bof_retrieval.cpp
+++ bof_retrieval.cpp
@@ -6,21 +6,57 @@
 #include <opencv2/xfeatures2d.hpp>
 #include "imageTrainer.h"
 
+#include <opencv2/flann/kdtree_index.h>
+
 
 using namespace std;
 using namespace cv;
 
+void kd_test(){
+
+    // Build kd tree
+    Point2f p1(2,3),   p2(5,4),p3(9, 6),  p4(4,7),  p5(8,1),p6(7, 2);
+    vector<cv::Point2f> source;
+    source.push_back(p1);
+    source.push_back(p2);
+    source.push_back(p3);
+    source.push_back(p4);
+    source.push_back(p5);
+    source.push_back(p6);
+    source.push_back(Point2f(1,1.5));
+    flann::KDTreeIndexParams indexParams(2);
+    flann::Index kdtree(cv::Mat(source).reshape(1), indexParams);
+
+    // Query
+    vector<float> query;
+    Point2f pt(0.1,1);
+    query.push_back(pt.x);
+    query.push_back(pt.y);
+    int k = 4; //number of nearest neighbors
+    vector<int> indices(k);//找到点的索引
+    vector<float> dists(k);
+
+    flann::SearchParams params(128);
+    kdtree.knnSearch(query, indices, dists, k,params);
+    cout<<indices[0]<< " : dist->" << dists[0] << " point:" << source[indices[0]] << endl;
+    cout<<indices[1]<<" : dist->" << dists[1] << " point:" << source[indices[1]] << endl;
+    cout<<indices[2]<<" : dist->" << dists[2] << " point:" << source[indices[2]] << endl;
+    cout<<indices[3]<<" : dist->" << dists[3] << " point:" << source[indices[3]] << endl;
+}
+
 int main()
 {
-    const string image_folder = "/home/brook/git_folder/image_retrieval/images";
+    /*const string image_folder = "/home/brook/git_folder/image_retrieval/images";
     vector<string> image_file_list;
     get_file_name_list(image_folder,image_file_list);
 
     ImageTrainer trainer(200,image_file_list);
 
     trainer.extract_sift();
-    trainer.vocabulary_kmeans();
+    trainer.vocabulary_kmeans();*/
 
+    kd_test();
+    
     
     return 0;
 }
--- imageRetrieval.cpp
+++ imageRetrieval.cpp
@@ -4,109 +4,15 @@
 
 #include <string>
 extern "C" {
-    #include <vl/sift.h>
     #include <vl/kmeans.h>
 }
+
+#include "vl_sift_detetor.h"
 
 using namespace std;
 using namespace cv;
 
-void bow()
-{
-    const string file = "";
-    Mat img = imread(file);
-    vector<KeyPoint> kpts;
-    Mat descriptors;
-
-    Ptr<xfeatures2d::SIFT> sift = xfeatures2d::SIFT::create();
-    sift->detectAndCompute(img,noArray(),kpts,descriptors);
-
-    // Trans sift to rootSift
-    for(int y = 0 ; y < descriptors.rows; y ++) {
-        for(int x = 0;  x < descriptors.cols; x ++) {
-            descriptors.at<float>(y,x) = sqrt(descriptors.at<float>(y,x));
-        }
-    }
-
-    float threshold = pow(10,12);
-    // L2 normalization
-    for(int y = 0; y < descriptors.rows; y ++) {
-        float sum = 0;
-        for(int x = 0; x < descriptors.cols; x ++) {
-            sum += descriptors.at<float>(y,x) * descriptors.at<float>(y,x);
-        }
-    }
-}
-
-/*
-    Extract sift using vlfeat
-    parameters:
-        vl_sfit, VlSiftFilt* 
-        data , image pixel data ,to be convert to float
-        kpts, keypoint list
-        descriptors, descriptor. Need to free the memory after using.
-*/
-void vl_sift_extract(VlSiftFilt *vl_sift, vl_sift_pix* data,
-                    vector<VlSiftKeypoint> &kpts,vector<float*> &descriptors) {
-    
-    // Detect keypoint and compute descriptor in each octave
-    if(vl_sift_process_first_octave(vl_sift,data) != VL_ERR_EOF){
-        while(true){
-            vl_sift_detect(vl_sift);
-
-            VlSiftKeypoint* pKpts = vl_sift->keys;
-            for(int i = 0; i < vl_sift->nkeys; i ++) {
-
-                double angles[4];
-                // 计算特征点的方向，包括主方向和辅方向，最多4个
-                int angleCount = vl_sift_calc_keypoint_orientations(vl_sift,angles,pKpts);
-
-                // 对于方向多于一个的特征点，每个方向分别计算特征描述符
-                // 并且将特征点复制多个
-                for(int i = 0 ; i < angleCount; i ++){
-                    float *des = new float[128];
-                    vl_sift_calc_keypoint_descriptor(vl_sift,des,pKpts,angles[0]);
-                    descriptors.push_back(des);
-                    kpts.push_back(*pKpts);
-                }
-                pKpts ++;
-            }    
-            // Process next octave
-            if(vl_sift_process_next_octave(vl_sift) == VL_ERR_EOF) {
-                break ;
-            }
-        }
-    }
-}
-
-void convert_sift_to_root_sift(vector<float*> &descriptors){
-        /*
-        Trans sift to rootSift
-        1. L1 normalize each descriptor(128-dims vector)
-        2. sqrt-root each element
-        3. [option] L2-normalize descriptor vector
-     */
-    for(float* ptr : descriptors) {
-        float sum = 0;
-        for(int i = 0; i < 128; i ++)
-            sum += *(ptr + i);
-        for(int i = 0; i < 128; i ++){
-            *(ptr + i) /= sum;
-            *(ptr + i) = sqrt(*(ptr + i));
-        }
-    }
-}
-
-void vl_root_sift_extract(VlSiftFilt *vl_sift, vl_sift_pix* data,vector<VlSiftKeypoint> &kpts,vector<float*> &descriptors) {
-
-    vl_sift_extract(vl_sift,data,kpts,descriptors);
-    convert_sift_to_root_sift(descriptors);
-}
-
-
-
-int main() 
-{
+void sift_test(){
     const string file = "../1.jpg";
     Mat img = imread(file,IMREAD_GRAYSCALE);
     Mat color_img = imread(file);
@@ -145,11 +51,14 @@ int main()
     cout << "vl_sift:" << descriptors.size() << endl;
     cout << "opencv_sift-descriptors:" << oc_des.size() << endl;
     
-    imwrite("/home/brook/Pictures/sift.jpg",color_img);
     namedWindow("SIFT");
     imshow("SIFT",color_img);
     waitKey();
+}
+
+
+int main() 
+{
+    
     return 0;
-
-
 }
